5dfd8e80160d13efb8daee31aa270071d18aca16
Aureliano Yepez <ayepez2020@berkeley.edu>
2018-12-14 11:36:15 -0800

    dfs

--- algorithms/islands/dfs_islands.cpp
+++ algorithms/islands/dfs_islands.cpp
@@ -1,15 +1,33 @@
+/*islands: given an n by n grid, each grid contains a '1'  
+ * or '0' char. '1' represent land, o/w '0' represents ocean.
+ * Find the number of islands in the n x n grid
+ * */
+
+// input :
+/* 1 | 0 | 0 |
+ * 0 | 1 | 1 |
+ * 1 | 0 | 0 |
+ */
+
+// output -> 3 
+
+//  note this approach uses Depth First Search 
+//  can be implemented with BFS and Union Find 
 class DFS {
 private:
     /*runtime is O(m+n)*/
     void dfs(vector<vector<char>>& grid, int r, int c) {
         
+        // number rows
         int nr = grid.size();
+    
+        // number columns 
         int nc = grid[0].size(); 
         
         /*set current grid to 0*/
         grid[r][c] = '0';
         /* check row down*/
-        if (r - 1 >= 0 && grid[r-1][c] == '1') dfs(grid, r - 1, c); 
+        /if (r - 1 >= 0 && grid[r-1][c] == '1') dfs(grid, r - 1, c); 
         /* check row up*/
         if (r + 1 < nr && grid[r+1][c] == '1') dfs(grid, r + 1, c); 
         /* check colum left */
@@ -41,4 +59,9 @@ public:
         }  
         return number_islands;         
     }
+}
+
+int main() {
+    
+    return 0; 
 };
