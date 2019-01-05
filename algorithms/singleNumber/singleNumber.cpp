455d7743d887e3c7fd717b4c31df8d8cb88a0d0b
Aureliano Yepez <ayepez2020@berkeley.edu>
2019-01-04 20:21:47 -0800

    more to add

---
+++ algorithms/singleNumber/singleNumber.cpp
@@ -0,0 +1,16 @@
+class SingleNumber {
+public:
+    // testing [1,2]
+    int singleNumber(vector<int>& nums) {
+     // xor all numbers since x^x = 0 and for all nums 
+     // the propery is communitive
+     int num = 0;
+     for (int i = 0; i < nums.size(); ++i) {
+         num ^= nums[i];
+     }
+     return num;
+    }
+};
+
+
+
