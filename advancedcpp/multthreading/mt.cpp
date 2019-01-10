ddaa5a5d4dedc27a018f4e12a9b07181ff313a68
Aureliano Yepez <ayepez2020@berkeley.edu>
2019-01-09 21:51:51 -0800

    signals

---
+++ advancedcpp/multthreading/mt.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <pthread.h>
+#include <cstdlib>
+
+
+// mt -> programs run concurrently
+// two types : process-based & thread based 
+/** 
+ *  process-based = concurrent execution of programs
+ *  thread-based = concurrent execution in same programs 
+ *  mt programs contain two or more parts each part is called a thread
+ *  each thread has a seperate execution path
+ */
+
+// program used posix threads 
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+
+    return 0;
+}
