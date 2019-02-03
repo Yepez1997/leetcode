60917b8703f97f4af7271c9232781f41f1f0d491
Aureliano Yepez <yepez4845@gmail.com>
2019-02-02 22:45:09 -0800

    more to commit

---
+++ algorithms/reverseLinkedList/reverseLL.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+
+using namespace std; 
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x): val(x) , next(NULL) {}
+};
+
+ListNode* reverseLinkedList(ListNode *head) {
+   
+
+    return head; 
+}
+
+
+
+// iterative have two pointers 
+// RUNNER SHOULD HAVE OUR REVERSED NODE 
+// RUNNER = NULL 
+// while l 
+// Runner = L 
+// 
+// L -> L.NEXT 
+// 
+
+
+int main() {
+    // first problem:
+    // input: 1->2->3->4->5->NULL
+    // output: 5->4->3->2->1->NULL
+    return 0;
+}
+
+
