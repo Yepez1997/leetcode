13979a7aed1719f977d917f6c2801fe40e26094d
Aureliano Yepez <yepez4845@gmail.com>
2019-02-05 12:36:24 -0800

    unique paths added again

---
+++ algorithms/uniquePathsI/uniquePaths.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std; 
+
+// find the number of unique paths from origin the bottom left
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        // init rows and cols 
+        int path[m + 1][n+1];
+        // init all colums and rows of the zero index to 1 
+        for (int i = 0; i < m; i++) {
+            path[i][0] = 1;
+        } 
+        
+        for (int j = 0; j < n; j++) {
+            path[0][j] = 1;
+        }
+        
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                path[i][j] = path[i - 1][j] + path[i][j - 1];
+        return path[m - 1][n - 1];
+    }
+};
+
+
+
