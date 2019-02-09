bc920fc2f030f69c91b97379648ee26452bb5207
Aureliano Yepez <yepez4845@gmail.com>
2019-02-08 20:19:30 -0800

    try to solve iteratively

--- exploreModules/recursionI/swapNodesLinkedList/swapAdjacentNodes.cpp
+++ exploreModules/recursionI/swapNodesLinkedList/swapAdjacentNodes.cpp
@@ -11,16 +11,20 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-
+/*Swaps pairs recursively in linked list*/
 ListNode* swapPairs(ListNode* head) {
         if(head == NULL) return NULL;
         if(head->next == NULL) return head;
         // set up a pointer to return
         ListNode* next = head->next;
+        // reverse the process & advance head pointer 
         head->next = swapPairs(next->next);
+        // point the next next pointer back to the front 
         next->next = head;
         return next;
     }
+
+    // iteratively with two pointers
 
 int main() {
     return 0; 
