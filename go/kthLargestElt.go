3287cd42561d0e99b89ffc5e5862a5c9617133df
Yepez1997 <yepez4845@gmail.com>
2019-06-18 11:25:56 -0700

    kth largest elt

---
+++ go/kthLargestElt.go
@@ -0,0 +1,30 @@
+import "container/heap"
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func findKthLargest(nums []int, k int) int {
+    h := make(IntHeap, 0)
+    h = append(h, nums...)
+	heap.Init(&h)
+    for i := 0; i < k-1; i++ {
+        heap.Pop(&h)
+    }
+    kthLargest := heap.Pop(&h)
+    return kthLargest.(int)
+}
