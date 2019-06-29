94cf80a16c47301c322233e010a01f1473063247
Yepez1997 <yepez4845@gmail.com>
2019-06-28 20:42:38 -0700

    inOrder traversal

---
+++ python/inOrder.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        inOrder = []
+        stack = []
+        while stack or root:
+          if root:
+            stack.append(root)
+            root = root.left
+          else:
+            node = stack.pop()
+            inOrder.append(node.val)
+            root = node.right
+        return inOrder
+
+
