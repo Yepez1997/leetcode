94d06dd5f3c44184222143b68814801fa23f596e
Yepez1997 <yepez4845@gmail.com>
2019-07-01 21:58:19 -0700

    merge k sorted

---
+++ python/mergeKSorted.py
@@ -0,0 +1,29 @@
+from Queue import PriorityQueue
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        head = point = ListNode(0)
+        pq = PriorityQueue()
+        # insert the first n then pop off the next n
+        for l in lists:
+            if l:
+                pq.put((l.val, l))
+
+        while not pq.empty():
+          val, node = pq.get()
+          point.next = ListNode(val)
+          point = point.next
+          node = node.next
+          if node:
+            pq.put((node.val, node))
+
+        return head.next
