f9fc7f0e57eeac598ab964cc0f461630e25805ef
Yepez1997 <yepez4845@gmail.com>
2019-07-19 18:12:19 -0700

    adding lcp

---
+++ python/longestPalindromicSubstring.py
@@ -0,0 +1,18 @@
+def longestPalindromicSubstring(string):
+    # Write your code here.
+	currentLongest = [0,1]
+	for i in range(1, len(string)):
+		odd = getLongestPalindrome(string, i-1, i+1)
+		even = getLongestPalindrome(string, i-1,i)
+		longest = max(odd, even, key = lambda x : x[1] - x[0])
+		currentLongest = max(currentLongest, longest, key = lambda x : x[1] - x[0])
+	return string[currentLongest[0]:currentLongest[1]]
+
+def getLongestPalindrome(string, leftindx, rightindx):
+	while leftindx >= 0 and rightindx < len(string):
+		if string[leftindx] != string[rightindx]:
+			break
+		leftindx -= 1
+		rightindx += 1
+	return [leftindx + 1, rightindx]
+
