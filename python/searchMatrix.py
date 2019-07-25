e43c024f4e5980496588042ea4662f67183187e6
Yepez1997 <yepez4845@gmail.com>
2019-07-25 16:52:32 -0700

    search matrix

---
+++ python/searchMatrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if matrix == []:
+          return False
+        row = 0
+        col = len(matrix[0])-1
+        while row < len(matrix) and col >= 0:
+          if matrix[row][col] > target:
+            col -= 1
+          elif matrix[row][col] < target:
+            row += 1
+          else:
+            return True
+        return False
