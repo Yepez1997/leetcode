8aec399d0453af2c8d4d2fa82bdae077cebfa54d
Yepez1997 <yepez4845@gmail.com>
2019-07-26 18:48:53 -0700

    has single cycle

---
+++ python/hasSingleCycle.py
@@ -0,0 +1,16 @@
+def hasSingleCycle(array):
+	numElementsSeen = 0
+	currentIndex = 0
+	while numElementsSeen < len(array):
+		# we have a cycle
+		if currentIndex == 0 and numElementsSeen > 0:
+			return False
+		numElementsSeen += 1
+		currentIndex = getNextIndex(currentIndex, array)
+	return currentIndex == 0
+
+def getNextIndex(i, array):
+	jump = array[i]
+	nextIdx = (i + jump) % len(array)
+	return nextIdx if nextIdx >= 0 else nextIdx + len(array)
+
