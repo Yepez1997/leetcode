495ea2b674ecf6d5f041e96f947337334fd74aab
Yepez1997 <yepez4845@gmail.com>
2019-07-28 14:54:18 -0700

    no adj

---
+++ python/maxSubSetNoAdj.py
@@ -0,0 +1,12 @@
+def maxSubsetSumNoAdjacent(array):
+    # Write your code here.
+	if not len(array):
+		return 0
+	elif len(array) == 1:
+		return array[0]
+	maxSums = [float('inf') for _ in array]
+	maxSums[0] = array[0]
+	maxSums[1] = max(array[0], array[1])
+	for i in range(2,len(array)):
+		maxSums[i] = max(maxSums[i-1], maxSums[i-2] + array[i])
+	return maxSums[-1]
