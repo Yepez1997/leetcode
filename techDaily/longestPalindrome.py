0493557cbc87428f7702bce8a8d8bd347e4476d1
Yepez1997 <yepez4845@gmail.com>
2019-07-30 18:07:37 -0700

    longes palindrome substring

---
+++ techDaily/longestPalindrome.py
@@ -0,0 +1,37 @@
+"""
+A palindrome is a sequence of characters that reads the same backwards and forwards.
+Given a string, s, find the longest palindromic substring in s.
+"""
+
+class Solution:
+    """ longestPalidrome - get the longest palindrome in a string """
+    def longestPalindrome(self, s):
+        longestPalindromeIndex = [0,1]
+        for i in range(1, len(s)):
+            oddLen = self.expandString(s, i-1, i+1)
+            evenLen = self.expandString(s, i-1, i)
+            currentLongestPalindromeIndex = max(oddLen, evenLen, key = lambda a: a[1] - a[0])
+            longestPalindromeIndex =  max(currentLongestPalindromeIndex, longestPalindromeIndex,  key = lambda a: a[1] - a[0])
+        # print(s[longestPalindromeIndex[0]: longestPalindromeIndex[1]])
+        return s[longestPalindromeIndex[0]: longestPalindromeIndex[1]]
+
+    """ expandString - expand from the current index"""
+    def expandString(self, s, leftIdx,rightIdx):
+        # should return coordintates
+        while leftIdx >= 0 and rightIdx < len(s):
+            if s[leftIdx] != s[rightIdx]:
+                # keep expanding the palindrome 
+                break
+            leftIdx -= 1
+            rightIdx += 1
+        return [leftIdx + 1, rightIdx]
+
+
+s = "tracecars"
+print(str(Solution().longestPalindrome(s)) == "racecar")
+t = "million"
+print(str(Solution().longestPalindrome(t)) == "illi")
+k = "banana"
+print(str(Solution().longestPalindrome(k)) == "anana")
+
+
