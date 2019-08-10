b1b5a88c5a239c36368bd8a90613036c44a58dc9
Yepez1997 <yepez4845@gmail.com>
2019-08-10 13:21:47 -0700

    left and right

--- techDaily/wordSearch.py
+++ techDaily/wordSearch.py
@@ -2,13 +2,12 @@
 or not the word target word exists in the matrix. Unlike a standard word search,
  the word must be either going left-to-right, or top-to-bottom in the matrix. '''
 
+ # word_search - check if a word exists by going strictly right or down
 def word_search(matrix, word):
     for row in range(len(matrix)):
         rowLen = len(matrix)
         for col in range(len(matrix[row])):
             coLen = len(matrix[row])
-            # needed to pass in the column and row lenghts to iterate them all through
-            # i was thinking dfs initially - but diagnals were'nt allwoed 
             search_right = searchRight(row, col, coLen, matrix)
             search_down = searchDown(row, col, rowLen, matrix)
             if word in (search_right, search_down):
@@ -16,13 +15,15 @@ def word_search(matrix, word):
     return False 
 
 
-
+# searchRight - traverse soley up to the end of the col
 def searchRight(row, col, length, matrix):
     word  = ""
     for i in range(length):
         word += matrix[row][i]
     return word 
 
+
+# searchDown - traverse soley up to the end of the row 
 def searchDown(row, col, length, matrix):
     word = ""
     for j in range(length):
